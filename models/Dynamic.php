d4d0f97580993feec995db8591475017c9e368ad
wangzhen <wangzhen@xywy.com>
2016-09-02 16:28:14 +0800

    12

---
+++ models/Dynamic.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangzhen
+ * Date: 2016/8/25
+ * Time: 16:22
+ */
+
+namespace app\models;
+
+
+class Dynamic
+{
+    public static function tableName()
+    {
+        return 'swmt_dynamic';
+    }
+}
